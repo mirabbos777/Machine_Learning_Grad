e19f9fbd99472bc7eed54c88a3e43f959ab49ae3
a2468834 <a2468834@gmail.com>
2020-07-03 15:54:46 +0800

    Update.

--- HW_Kaggle/LogReg.py
+++ HW_Kaggle/LogReg.py
@@ -6,7 +6,7 @@ import sys
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.decomposition import PCA
-from sklearn.model_selection import ParameterGrid, GridSearchCV, train_test_split
+from sklearn.model_selection import ParameterGrid, train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 
@@ -55,17 +55,27 @@ y_train = numpy.array(y_train, dtype='float')
 X_test = numpy.array(X_test, dtype='float')
 
 
-# Calculate Pearson correlation coefficient of y_train and every X_train's column
-# Drop out columns of X_train which have too small (<0.005) correlation coefficient
-delete_column_num = []
-cov_matrix = numpy.cov(y_train, X_train, rowvar=False)
-for i in range(1, cov_matrix.shape[0]):
-    r = cov_matrix[0][i]/(numpy.std(y_train)*numpy.std(X_train[:, i-1]))
-    if r < 0.005:
-        delete_column_num.append(i-1)
-# Delete columns of training data and testing data simultaneously
-X_train = numpy.delete(X_train, delete_column_num, 1)
-X_test = numpy.delete(X_test, delete_column_num, 1)
+# (Mode 1)  Delete useless columns by calculating Pearson r
+#           Calculate Pearson correlation coefficient of y_train and every X_train's column
+#           Drop out columns of X_train which have too small (<0.005) correlation coefficient
+# (Mode 2)  Delete useless columns by LB probing 
+#           Keep only important features found by LB probing
+# (Mode 3)  Do nothing
+if sys.argv[1] == 1:  # Mode 1
+    delete_column_num = []
+    cov_matrix = numpy.cov(y_train, X_train, rowvar=False)
+    for i in range(1, cov_matrix.shape[0]):
+        r = cov_matrix[0][i]/(numpy.std(y_train)*numpy.std(X_train[:, i-1]))
+        if r < 0.005:
+            delete_column_num.append(i-1)
+    # Delete columns of training data and testing data simultaneously
+    X_train = numpy.delete(X_train, delete_column_num, 1)
+    X_test = numpy.delete(X_test, delete_column_num, 1)
+elif sys.argv[1] == 2:  # Mode 2
+    remain_column = [16, 29, 33, 45, 63, 65, 70, 73, 91, 106, 108, 117, 132, 164, 189, 199, 209, 217, 239]
+    delete_column = [i for i in range(0, 300) if i not in remain_column]
+    X = numpy.delete(X, delete_column, 1)
+    X_test = numpy.delete(X_test, delete_column, 1)
 
 
 # Standardize both of training and testing data part by each feature (i.e., columns of matrix X_train and X_test)
@@ -73,43 +83,39 @@ ss = StandardScaler()
 X_train = ss.fit_transform(X_train)
 X_test = ss.transform(X_test)  # Apply "the same" standardizing "pattern" on testing data
 
-'''
+
 # Use SMOTE oversampling positive instances (y_train==1) into the same number of negative instances (y_train==0)
 ros = RandomOverSampler(random_state=0)
 X_train, y_train = ros.fit_resample(X_train, y_train)
 
 
-# Apply PCA to matrix X_train (# of components >= 82 would be better)
-pca = PCA()
-X_train = pca.fit_transform(X_train)
-X_test = pca.transform(X_test)
-'''
-
-# Find optimal C (MAX AUC value) by training models using 5-fold cross-validation
+# Find optimal parameters of training models using 5-fold cross-validation
 X_train_train, X_train_test, y_train_train, y_train_test = train_test_split(X_train, y_train, test_size=0.2)
 max_auc = 0
 opt_param = 0
 param_grid = {
-                'C_list': numpy.arange(0.01, 1.0, 0.01).tolist()
+                'C_list': numpy.arange(0.01, 1.0, 0.01).tolist(),
+                'Norm_list': ['l1', 'l2']
 }
 param_grid = list(ParameterGrid(param_grid))
 for i in param_grid:
-    print("# of rounds: %d" % param_grid.index(i))
-    clf = LogisticRegression(class_weight='balanced', solver='liblinear', penalty='l1', C=i['C_list'], max_iter=10000)
+    clf = LogisticRegression(class_weight='balanced', solver='liblinear', penalty=i['Norm_list'], C=i['C_list'], max_iter=10000)
     clf.fit(X_train_train, y_train_train)
-    fpr, tpr, skip = metrics.roc_curve(y_train_test, clf.predict_proba(X_train_test)[:, 1])
+    temp_result = clf.predict_proba(X_train_test)[:, 1]
+    fpr, tpr, skip = metrics.roc_curve(y_train_test, temp_result)
     temp_auc = metrics.auc(fpr, tpr)
     if max_auc < temp_auc:
         max_auc = temp_auc
-        opt_param = i['C_list']
+        opt_param = i
 
-print("Optimal parameters: %f" % opt_param)
-print("Maximum AUC: %f" % max_auc)
 
-clf = LogisticRegression(class_weight='balanced', solver='liblinear', penalty='l1', C=opt_param, max_iter=10000)
+# Train model by optimal parameters
+clf = LogisticRegression(class_weight='balanced', solver='liblinear', penalty=opt_param['Norm_list'], C=opt_param['C_list'], max_iter=10000)
 clf.fit(X_train, y_train)
 target_column = clf.predict_proba(X_test)[:, 1]
 
+
+# Output results into a csv file
 title_line = numpy.array(["id", "target"])
 title_line = numpy.reshape(title_line, (1, 2))
 
@@ -122,7 +128,7 @@ infptr_1.close()
 infptr_2.close()
 outfptr.close()
 
+
 # Print program executing time
 print("CSV file name: " + file_name)
-print("ExeTime: %.3f"%((time.time_ns()-start) / (10**9)))
-#numpy.savetxt(outfptr, numpy.delete(X_train, delete_column_num, 1), '%s', ',')
+print("ExeTime: %.3f" % ((time.time_ns()-start) / (10**9)))
